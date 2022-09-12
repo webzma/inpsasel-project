2581c43230378211ab28b1f756c7642e5e4c81ca
webzma <ledezma.wilberk@gamil.com>
2022-09-12 12:47:56 -0400

    Agregué un nuevo archivo llamado registro y agregué cambios en el formulario del login

--- js/login.js
+++ js/login.js
@@ -12,31 +12,70 @@ document.addEventListener("DOMContentLoaded", () => {
 // Función que se ejecuta después que hagamos click en el btn del formulario
 function action(event) {
   event.preventDefault();
-  if (name.value.length < 5 || password.value.length < 5 || isMayus(password.value[0]) === false || isNumber(password.value) === undefined) { 
-    const htmlCode = 
+
+  let htmlCode = "";
+  const container = document.querySelector(".container-father-error");
+
+  if (name.value.length < 6) {
+    let htmlCode = 
       `<div class="container-error">
-        <p>Ingrese correctamente su contraseña o usuario</p>
+        <p>El usuario debe tener al menos seis carácteres</p>
       </div>`; 
-      document.querySelector(".container-father-error").innerHTML = htmlCode;
-  } else {
+    container.innerHTML = htmlCode;  
+  }
+
+  else if (password.value.length < 5) {
+    htmlCode = 
+      `<div class="container-error">
+        <p>La contraseña debe tener al menos cinco carácteres</p>
+      </div>`; 
+    container.innerHTML = htmlCode;       
+  } 
+
+  else if (isMayus(password.value) === undefined) {
+    htmlCode = 
+      `<div class="container-error">
+        <p>La contraseña debe tener al menos un carácter en mayúscula</p>
+      </div>`; 
+    container.innerHTML = htmlCode;           
+  }
+
+  else if (isNumber(password.value) === undefined) {
+    htmlCode = 
+      `<div class="container-error">
+        <p>La contraseña debe tener al menos un número</p>
+      </div>`; 
+    container.innerHTML = htmlCode;                 
+  }
+
+  else {
     document.querySelector(".container-father-error").innerHTML = "";
     Swal.fire({
       icon: "success",
       title: "¡Success!",
+    });
+
+    const buttonAlert = document.querySelector(".swal2-confirm");
+    buttonAlert.addEventListener("click", () => {
+      window.location.pathname = "/index.html";
     });
   }
 }
 
 // Se encarga de validar si en la clave hay una letra en mayúscula
 function isMayus(caracter) {
-  return caracter === caracter.toUpperCase();
+  for (const letter of caracter) {
+    if(letter === letter.toUpperCase() && !Number(letter)) return true
+  }  
 }
+
 
 // Se encarga de determinar si en la clave hay números
 function isNumber(caracter) {
   for (var i = 0; i < caracter.length; i++) {
     let element = caracter[i];
-    if (Number(element)) {
+    if (Number(element) || element == 0) {
+      console.log(element)
       return true;
     }
   }
